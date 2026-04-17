09186c51d6c57492cf7a27ae46912edaadebd2e4
rawnnaq5 <rawnaqmarae947@gmail.com>
2026-04-17 16:42:22 +0300

    add header footer and routing

---
+++ Uni_Stay/src/components/Footer/FooterPages.jsx
@@ -0,0 +1,14 @@
+import "./FooterPages.css";
+
+function FooterPages() {
+  return (
+    <footer className="pages-footer">
+      <div className="pages-footer-content">
+        <p>© 2026 UniStay . جميع الحقوق محفوظة</p>
+        <p>صُمم بعناية لطلاب جامعة النجاح الوطنية</p>
+      </div>
+    </footer>
+  );
+}
+
+export default FooterPages;
---
+++ Uni_Stay/src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom'
+import './Header.css'
+import logoImage from '../../assets/images/logo.jpeg'
+function Header() {
+  return (
+    <header className="navbar">
+      <div className="navbar-container">
+
+        <div className="nav-right">
+          <Link to="/" className="logo-box">
+            <img src={logoImage} alt="logo" className="logo-image" />
+            <span className="logo">UniStay</span>
+          </Link>
+        </div>
+
+        <nav className="nav-center">
+          <ul className="nav-links">
+            <li><Link to="/">الرئيسية</Link></li>
+            <li><a href="/#search-section">ابحث عن سكن</a></li>
+            <li><a href="/#why-us">لماذا تختار منصتنا</a></li>
+            <li><a href="/#how-it-works">كيف تعمل منصتنا</a></li>
+          </ul>
+        </nav>
+
+        <div className="nav-left">
+          <Link to="/student" className="login-link">تسجيل الدخول</Link>
+          <Link to="/owner" className="add-btn">أضف عقارك</Link>
+        </div>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
