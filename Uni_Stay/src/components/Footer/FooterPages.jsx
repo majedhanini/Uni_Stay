dcff3fa5993469941522629bfe4dfe89f3551a20
majedhanini <najedalhanini604@gmail.com>
2026-04-17 17:17:22 +0300

    SCRUM-16 Shared Layout Components

--- Uni_Stay/src/components/Footer/FooterPages.jsx
+++ Uni_Stay/src/components/Footer/FooterPages.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import logoImage from "../../assets/images/5409f3b3-b085-4d42-8652-580678be8b8f.jpg";
 
 function Header() {
+
   return (
     <header className="navbar">
       <div className="navbar-container">
